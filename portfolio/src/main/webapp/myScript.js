b28489cf3f001a7cfa1c64504740d10bcfd1213b
MaleeS <m.seechan@gmail.com>
2021-06-15 19:32:47 +0000

    Added feature to Index page that fetches random facts

--- portfolio/src/main/webapp/myScript.js
+++ portfolio/src/main/webapp/myScript.js
@@ -1,8 +1,13 @@
 /** Fetches a random fact from the server and adds it to the page. */
 async function showFetchTest() {
     const serverResponse = await fetch('/hello');
-    const responseText = await serverResponse.text();
+//server returns a json object
+    const responseJson = await serverResponse.json();
+    console.log(responseJson);
+    //returns array of responseJson's values
+    const retArr = Object.values(responseJson);
+    const randIndex = Math.floor(Math.random() * retArr.length);
 
     const factContainer = document.getElementById('fetch-container');
-    factContainer.innerText = responseText;
+    factContainer.innerHTML = retArr[randIndex];
 }
