095aac63d853c3775ed90b9234c9d7309f87b3c9
MaleeS <m.seechan@gmail.com>
2021-06-19 20:29:49 +0000

    auto-reformatted file and deleted theia folder

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -9,19 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String nameValue = request.getParameter("contact-name");
-    String emailValue = request.getParameter("contact-email");
-    String messageValue = request.getParameter("contact-message");
+        // Get the value entered in the form.
+        String nameValue = request.getParameter("contact-name");
+        String emailValue = request.getParameter("contact-email");
+        String messageValue = request.getParameter("contact-message");
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + nameValue + " |Email: " + emailValue + " |Message: " + messageValue);
+        // Print the value so you can see it in the server logs.
+        System.out.println("You submitted: " + nameValue + " |Email: " + emailValue + " |Message: " + messageValue);
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + messageValue);
-    response.sendRedirect("contact.html");
-  }
+        // Write the value to the response so the user can see it.
+        response.getWriter().println("You submitted: " + messageValue);
+        response.sendRedirect("contact.html");
+    }
 }
--- portfolio/src/main/webapp/myScript.js
+++ portfolio/src/main/webapp/myScript.js
@@ -1,13 +1,14 @@
 /** Fetches a random fact from the server and adds it to the page. */
-$(document).ready(function(){
-    $("#nav-bar").load("/nav.html")});
+$(document).ready(function () {
+    $("#nav-bar").load("/nav.html")
+});
 
 async function showFetchTest() {
     const serverResponse = await fetch('/hello');
-//server returns a json object
+
     const responseJson = await serverResponse.json();
     console.log(responseJson);
-    //returns array of responseJson's values
+
     const retArr = Object.values(responseJson);
     const randIndex = Math.floor(Math.random() * retArr.length);
 
